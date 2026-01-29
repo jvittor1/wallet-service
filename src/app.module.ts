1ef06e7a57b3442766814166deac4128ddff9ae3
João Vitor <joaovitorb3105@gmail.com>
2026-01-28 23:38:49 -0300

    create entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, TransactionModule, WalletModule],
 })
 export class AppModule {}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  password: string;
+}
